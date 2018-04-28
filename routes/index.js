9e793bea2f255ebee414f97a3a1ab24ab37f80c5
kdncode <kdncode@gmail.com>
2018-04-27 23:28:27 -0500

    added multer to upload img

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var multer = require("multer")
+
+// (Using Multer to upload file) - Step 2 
+// Indicate the upload path & file name
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' +  file.originalname)
+  }
+})
+
+var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* POST home page (form) */
+router.post('/', upload.single('productImage'), function(req, res, next) {
+  var myData = req.body.tdsp;
+  res.send("I got the post " + myData);
 });
 
 module.exports = router;
